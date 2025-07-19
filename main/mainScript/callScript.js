8acad89b8b0fad4b5108f99d27f0dae260d86c34
Dante Hawler <202559257+D-Hawler@users.noreply.github.com>
2025-07-19 12:50:05 +0300

    Board design and board interaction

--- main/mainScript/callScript.js
+++ main/mainScript/callScript.js
@@ -1,3 +1,37 @@
 
 
 document.addEventListener('click', () => {});
+
+
+
+
+
+
+
+
+
+
+
+
+(function () {
+    const bord = document.querySelectorAll('.bord');
+
+    const cord = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    bord.forEach((bord) => {
+        cord.forEach((row) => {
+        let col = 0;
+        
+        for (let i = 0; i < 10; i ++) {
+                const cell = document.createElement('div')
+                cell.dataset.row = row;
+                cell.dataset.col = col;
+                cell.dataset.status = 'empty';
+
+                col += 1;
+
+                bord.appendChild(cell);
+            };
+        });
+    });
+})();
